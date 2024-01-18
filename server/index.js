1879655b768e260192d2113fa442638d44921458
Abderrahmen Dai <abderrahmen.dai.11@gmail.com>
2024-01-18 02:35:38 +0100

    add auth and login controller

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cros from "cros";
+import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import path from "path";
-import { fileUrlPath } from "url";
+import path from 'path';
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /** CONFIGURATION **/
 const __filename = fileURLToPath(import.meta.url);
@@ -35,9 +37,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /** ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+// app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-
-/!! ROUTES WITH FILES */
+/* ROUTES */
+app.use("/auth", authRoutes);
+// app.use("/users", userRoutes);
+// app.use("/posts", postRoutes);
 
 /** MONGOOSE SETUP **/
 
